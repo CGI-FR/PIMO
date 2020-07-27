6083ba215e82201c837473bd9363448ed70eb73f
Alexis Piaud <alexis.piaud@cgi.com>
2020-07-27 09:13:45 +0000

    refactor(weightedchoice): recreating weightedrand

--- pkg/weightedchoice/weightedchoice.go
+++ pkg/weightedchoice/weightedchoice.go
@@ -2,25 +2,61 @@ package weightedchoice
 
 import (
 	"math/rand"
-
-	wr "github.com/mroth/weightedrand"
+	"sort"
 
 	"makeit.imfr.cgi.com/makeit2/scm/lino/pimo/pkg/model"
 )
 
+// WeightedChoice adapted from github.com/mroth/weightedrand
+
+//Choice is a wrapper of item and its weight
+type Choice struct {
+	Item   interface{}
+	Weight uint
+}
+
+//A Chooser is to choose a Choice by weight
+type Chooser struct {
+	data   []Choice
+	totals []int
+	max    int
+	rand   *rand.Rand
+}
+
+// NewChooser creates a Chooser from Choices
+func NewChooser(seed int64, cs ...Choice) Chooser {
+	sort.Slice(cs, func(i, j int) bool {
+		return cs[i].Weight < cs[j].Weight
+	})
+	totals := make([]int, len(cs))
+	runningTotal := 0
+	for i, c := range cs {
+		runningTotal += int(c.Weight)
+		totals[i] = runningTotal
+	}
+	return Chooser{data: cs, totals: totals, max: runningTotal, rand: rand.New(rand.NewSource(seed))}
+}
+
+// Pick returns a choice from a Chooser
+func (chs Chooser) Pick() interface{} {
+	r := chs.rand.Intn(chs.max) + 1
+	i := sort.SearchInts(chs.totals, r)
+	return chs.data[i].Item
+}
+
 // MaskEngine a list of masking value with weight for random
 type MaskEngine struct {
-	cs wr.Chooser
+	cs Chooser
 }
 
 // NewMask returns a WeightedMaskList from slice of model.Entry and weights
 func NewMask(list []model.WeightedChoiceType, seed int64) MaskEngine {
 	rand.Seed(seed)
-	cs := []wr.Choice{}
+	cs := []Choice{}
 	for k := range list {
-		cs = append(cs, wr.Choice{Item: list[k].Choice, Weight: list[k].Weight})
+		cs = append(cs, Choice{Item: list[k].Choice, Weight: list[k].Weight})
 	}
-	return MaskEngine{wr.NewChooser(cs...)}
+	return MaskEngine{NewChooser(seed, cs...)}
 }
 
 // Mask choose a mask value randomly
