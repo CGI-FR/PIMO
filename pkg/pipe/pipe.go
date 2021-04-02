5fbb2a978c6b45c879ffc1a3bf27e4f13023021e
aurya <adrien.aury@cgi.com>
2021-04-02 19:54:49 +0000

    wip: feat(pipe) poc (warning code is dirty)

--- pkg/pipe/pipe.go
+++ pkg/pipe/pipe.go
@@ -18,16 +18,126 @@
 package pipe
 
 import (
-	"fmt"
+	"errors"
+	"strconv"
 
+	"github.com/cgi-fr/pimo/pkg/add"
+	"github.com/cgi-fr/pimo/pkg/command"
+	"github.com/cgi-fr/pimo/pkg/constant"
+	"github.com/cgi-fr/pimo/pkg/dateparser"
+	"github.com/cgi-fr/pimo/pkg/duration"
+	"github.com/cgi-fr/pimo/pkg/ff1"
+	"github.com/cgi-fr/pimo/pkg/fluxuri"
+	"github.com/cgi-fr/pimo/pkg/hash"
+	"github.com/cgi-fr/pimo/pkg/increment"
 	"github.com/cgi-fr/pimo/pkg/model"
+	"github.com/cgi-fr/pimo/pkg/randdate"
+	"github.com/cgi-fr/pimo/pkg/randdura"
+	"github.com/cgi-fr/pimo/pkg/randomdecimal"
+	"github.com/cgi-fr/pimo/pkg/randomint"
+	"github.com/cgi-fr/pimo/pkg/randomlist"
+	"github.com/cgi-fr/pimo/pkg/rangemask"
+	"github.com/cgi-fr/pimo/pkg/regex"
+	"github.com/cgi-fr/pimo/pkg/remove"
+	"github.com/cgi-fr/pimo/pkg/replacement"
+	"github.com/cgi-fr/pimo/pkg/templatemask"
+	"github.com/cgi-fr/pimo/pkg/weightedchoice"
 )
+
+type source struct {
+	value model.Dictionary
+	read  *bool
+}
+
+func (s source) Open() error {
+	return nil
+}
+
+func (s source) Err() error {
+	return nil
+}
+
+func (s source) Next() bool {
+	return !*s.read
+}
+
+func (s source) Value() model.Dictionary {
+	defer func() { *s.read = true }()
+	return s.value
+}
+
+func injectMaskFactories() []model.MaskFactory {
+	return []model.MaskFactory{
+
+		constant.Factory,
+		command.Factory,
+		randomlist.Factory,
+		randomint.Factory,
+		weightedchoice.Factory,
+		regex.Factory,
+		hash.Factory,
+		randdate.Factory,
+		increment.Factory,
+		replacement.Factory,
+		duration.Factory,
+		templatemask.Factory,
+
+		rangemask.Factory,
+		randdura.Factory,
+		randomdecimal.Factory,
+		dateparser.Factory,
+		ff1.Factory,
+		Factory,
+	}
+}
+
+func injectMaskContextFactories() []model.MaskContextFactory {
+	return []model.MaskContextFactory{
+		fluxuri.Factory,
+		add.Factory,
+		remove.Factory,
+	}
+}
 
 // MaskEngine is a value that always mask the same way
 type MaskEngine struct {
 	masking      []model.Masking
 	injectParent string
 	injectRoot   string
+}
+
+func BuildPipeline(pipeline model.Pipeline, masking []model.Masking) (model.Pipeline, error) {
+	maskFactories := injectMaskFactories()
+	maskContextFactories := injectMaskContextFactories()
+	for _, v := range masking {
+		nbArg := 0
+
+		for _, factory := range maskFactories {
+			mask, present, err := factory(v, 0)
+			if err != nil {
+				return nil, errors.New(err.Error() + " for " + v.Selector.Jsonpath)
+			}
+			if present {
+				pipeline = pipeline.Process(model.NewMaskEngineProcess(model.NewPathSelector(v.Selector.Jsonpath), mask))
+				nbArg++
+			}
+		}
+
+		for _, factory := range maskContextFactories {
+			mask, present, err := factory(v, 0)
+			if err != nil {
+				return nil, errors.New(err.Error() + " for " + v.Selector.Jsonpath)
+			}
+			if present {
+				pipeline = pipeline.Process(model.NewMaskContextEngineProcess(model.NewPathSelector(v.Selector.Jsonpath), mask))
+				nbArg++
+			}
+		}
+		if nbArg != 1 {
+			return pipeline, errors.New("Not the right number of argument for " + v.Selector.Jsonpath + ". There should be 1 and there is " + strconv.Itoa(nbArg))
+		}
+	}
+	return pipeline, nil
 }
 
 // NewMask return a MaskEngine from a value
@@ -37,8 +147,13 @@ func NewMask(injectParent string, injectRoot string, masking ...model.Masking) M
 
 // Mask return a Constant from a MaskEngine
 func (pipem MaskEngine) Mask(e model.Entry, context ...model.Dictionary) (model.Entry, error) {
-	fmt.Println("I need to mask", e)
-	return nil, nil
+	read := false
+	source := source{InterfaceToDictionaryEntry(e), &read}
+	pipeline := model.NewPipeline(source)
+	pipeline, _ = BuildPipeline(pipeline, pipem.masking)
+	var result []model.Dictionary
+	pipeline.AddSink(model.NewSinkToSlice(&result)).Run()
+	return result[0], nil
 }
 
 // Factory create a mask from a configuration
@@ -47,4 +162,33 @@ func Factory(conf model.Masking, seed int64) (model.MaskEngine, bool, error) {
 		return NewMask(conf.Mask.Pipe.InjectParent, "nil", conf.Mask.Pipe.Masking...), true, nil
 	}
 	return nil, false, nil
+}
+
+// InterfaceToDictionary returns a model.Dictionary from an interface
+func InterfaceToDictionaryEntry(inter interface{}) model.Dictionary {
+	dic := make(map[string]model.Entry)
+	mapint := inter.(map[string]model.Entry)
+
+	for k, v := range mapint {
+		switch typedValue := v.(type) {
+		case map[string]interface{}:
+			dic[k] = model.InterfaceToDictionary(v)
+		case []interface{}:
+			tab := []model.Entry{}
+			for _, item := range typedValue {
+				_, dico := item.(map[string]interface{})
+
+				if dico {
+					tab = append(tab, model.InterfaceToDictionary(item))
+				} else {
+					tab = append(tab, item)
+				}
+			}
+			dic[k] = tab
+		default:
+			dic[k] = v
+		}
+	}
+
+	return dic
 }
