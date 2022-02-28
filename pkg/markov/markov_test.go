5e14e8300e6b0c69f02e0d68426f63654267dd40
paul.briand <paul.briand@cgi.com>
2022-02-28 15:57:15 +0000

    test(markov): add unit test

--- pkg/markov/markov_test.go
+++ pkg/markov/markov_test.go
@@ -1,6 +1,8 @@
 package markov
 
 import (
+	"math/rand"
+	"strings"
 	"testing"
 
 	"github.com/cgi-fr/pimo/pkg/model"
@@ -12,4 +14,18 @@ func TestFactoryShouldCreateAMask(t *testing.T) {
 	_, present, err := Factory(maskingConfig, 0, nil)
 	assert.True(t, present, "should be true")
 	assert.Nil(t, err, "error should be nil")
+}
+
+func TestFactoryShouldCreateAChain(t *testing.T) {
+	//nolint: gosec
+	chain := NewChain(2, rand.New(rand.NewSource(42)), "")
+	data := []string{"aaa", "bbb", "aba"}
+	for _, el := range data {
+		chain.Add(strings.Split(el, ""))
+	}
+	newState, err := chain.Generate([]string{"a", "b"})
+	assert.Nil(t, err, "error should be nil")
+	assert.Equal(t, "a", newState)
+	_, err = chain.Generate([]string{"a"})
+	assert.Error(t, err, "N-gram length does not match chain order")
 }
