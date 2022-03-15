f37ca15fbabbdb2787435bc6f7b599f5e1a6de6c
Adrien Aury <44274230+adrienaury@users.noreply.github.com>
2022-03-15 21:33:38 +0100

    refactor: prepare for play (#100)
    * refactor(play): move some code
    
    * refactor(play): fix regressions
    
    * chore: debug github action
    
    * chore: revert debug github action
    
    * feat(play): backend

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/alecthomas/jsonschema v0.0.0-20210526225647-edb03dcab7bc
 	github.com/capitalone/fpe v1.2.1
 	github.com/goccy/go-yaml v1.9.5
+	github.com/labstack/echo/v4 v4.7.0
 	github.com/mattn/go-isatty v0.0.14
 	github.com/rs/zerolog v1.26.1
 	github.com/spf13/cobra v1.4.0
@@ -22,12 +23,14 @@ require (
 	github.com/Masterminds/semver/v3 v3.1.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.13.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/google/gxui v0.0.0-20151028112939-f85e0a97b3a4 // indirect
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/huandu/xstrings v1.3.1 // indirect
 	github.com/iancoleman/orderedmap v0.0.0-20190318233801-ac98e3ecb4b0 // indirect
 	github.com/imdario/mergo v0.3.11 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mitchellh/copystructure v1.0.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.0 // indirect
@@ -36,8 +39,12 @@ require (
 	github.com/smartystreets/goconvey v1.6.4 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20211215165025-cf75a172585e // indirect
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
 	golang.org/x/sys v0.0.0-20211205182925-97ca703d548d // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- internal/app/pimo/pimo.go
+++ internal/app/pimo/pimo.go
@@ -20,16 +20,183 @@ package pimo
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
+	"regexp"
+	"time"
 
+	over "github.com/Trendyol/overlog"
 	"github.com/alecthomas/jsonschema"
+	"github.com/cgi-fr/pimo/pkg/add"
+	"github.com/cgi-fr/pimo/pkg/addtransient"
+	"github.com/cgi-fr/pimo/pkg/command"
+	"github.com/cgi-fr/pimo/pkg/constant"
+	"github.com/cgi-fr/pimo/pkg/dateparser"
+	"github.com/cgi-fr/pimo/pkg/duration"
+	"github.com/cgi-fr/pimo/pkg/ff1"
+	"github.com/cgi-fr/pimo/pkg/fluxuri"
+	"github.com/cgi-fr/pimo/pkg/fromjson"
+	"github.com/cgi-fr/pimo/pkg/hash"
+	"github.com/cgi-fr/pimo/pkg/increment"
 	"github.com/cgi-fr/pimo/pkg/jsonline"
+	"github.com/cgi-fr/pimo/pkg/luhn"
+	"github.com/cgi-fr/pimo/pkg/markov"
 	"github.com/cgi-fr/pimo/pkg/model"
+	"github.com/cgi-fr/pimo/pkg/pipe"
+	"github.com/cgi-fr/pimo/pkg/randdate"
+	"github.com/cgi-fr/pimo/pkg/randdura"
+	"github.com/cgi-fr/pimo/pkg/randomdecimal"
+	"github.com/cgi-fr/pimo/pkg/randomint"
+	"github.com/cgi-fr/pimo/pkg/randomlist"
+	"github.com/cgi-fr/pimo/pkg/randomuri"
+	"github.com/cgi-fr/pimo/pkg/rangemask"
+	"github.com/cgi-fr/pimo/pkg/regex"
+	"github.com/cgi-fr/pimo/pkg/remove"
+	"github.com/cgi-fr/pimo/pkg/replacement"
+	"github.com/cgi-fr/pimo/pkg/statistics"
+	"github.com/cgi-fr/pimo/pkg/templateeach"
+	"github.com/cgi-fr/pimo/pkg/templatemask"
+	"github.com/cgi-fr/pimo/pkg/weightedchoice"
+	"github.com/rs/zerolog/log"
 )
 
 type CachedMaskEngineFactories func(model.MaskEngine) model.MaskEngine
 
-func DumpCache(name string, cache model.Cache, path string, reverse bool) error {
+type Config struct {
+	SingleInput      *model.Dictionary
+	EmptyInput       bool
+	RepeatUntil      string
+	RepeatWhile      string
+	Iteration        int
+	SkipLineOnError  bool
+	SkipFieldOnError bool
+	CachesToDump     map[string]string
+	CachesToLoad     map[string]string
+}
+
+type Context struct {
+	cfg                 Config
+	pdef                model.Definition
+	pipeline            model.Pipeline
+	source              model.Source
+	repeatCondition     string
+	repeatConditionMode string
+	caches              map[string]model.Cache
+}
+
+func NewContext(pdef model.Definition) Context {
+	return Context{pdef: pdef}
+}
+
+func (ctx *Context) Configure(cfg Config) error {
+	log.Info().
+		Bool("skipLineOnError", cfg.SkipLineOnError).
+		Bool("skipFieldOnError", cfg.SkipFieldOnError).
+		Int("repeat", cfg.Iteration).
+		Bool("empty-input", cfg.EmptyInput).
+		Interface("dump-cache", cfg.CachesToDump).
+		Interface("load-cache", cfg.CachesToLoad).
+		Msg("Start PIMO")
+
+	ctx.cfg = cfg
+
+	over.AddGlobalFields("context")
+	switch {
+	case cfg.EmptyInput:
+		over.MDC().Set("context", "empty-input")
+		ctx.source = model.NewSourceFromSlice([]model.Dictionary{{}})
+	case cfg.SingleInput != nil:
+		over.MDC().Set("context", "single-input")
+		ctx.source = model.NewSourceFromSlice([]model.Dictionary{*cfg.SingleInput})
+	default:
+		over.MDC().Set("context", "stdin")
+		ctx.source = jsonline.NewSource(os.Stdin)
+	}
+
+	if cfg.RepeatUntil != "" && cfg.RepeatWhile != "" {
+		return fmt.Errorf("Cannot use repeatUntil and repeatWhile options together")
+	}
+
+	ctx.repeatCondition = cfg.RepeatWhile
+	ctx.repeatConditionMode = "while"
+	if cfg.RepeatUntil != "" {
+		ctx.repeatCondition = cfg.RepeatUntil
+		ctx.repeatConditionMode = "until"
+	}
+
+	if ctx.repeatCondition != "" {
+		ctx.source = model.NewTempSource(ctx.source)
+	}
+
+	ctx.pipeline = model.NewPipeline(ctx.source).
+		Process(model.NewCounterProcessWithCallback("input-line", 0, updateContext)).
+		Process(model.NewRepeaterProcess(cfg.Iteration))
+	over.AddGlobalFields("input-line")
+
+	model.InjectMaskContextFactories(injectMaskContextFactories())
+	model.InjectMaskFactories(injectMaskFactories())
+	model.InjectConfig(cfg.SkipLineOnError, cfg.SkipFieldOnError)
+
+	var err error
+	ctx.pipeline, ctx.caches, err = model.BuildPipeline(ctx.pipeline, ctx.pdef, nil)
+	if err != nil {
+		return fmt.Errorf("Cannot build pipeline: %w", err)
+	}
+
+	if ctx.repeatCondition != "" {
+		processor, err := model.NewRepeaterUntilProcess(ctx.source.(*model.TempSource), ctx.repeatCondition, ctx.repeatConditionMode)
+		if err != nil {
+			return fmt.Errorf("Cannot build pipeline: %w", err)
+		}
+		ctx.pipeline = ctx.pipeline.Process(processor)
+	}
+
+	return nil
+}
+
+func (ctx *Context) Execute(out io.Writer) (statistics.ExecutionStats, error) {
+	for name, path := range ctx.cfg.CachesToLoad {
+		cache, ok := ctx.caches[name]
+		if !ok {
+			return statistics.WithErrorCode(2), fmt.Errorf("Cache not found: %v", name)
+		}
+		if err := loadCache(name, cache, path, ctx.pdef.Caches[name].Reverse); err != nil {
+			return statistics.WithErrorCode(3), fmt.Errorf("Cannot load cache: %v", err)
+		}
+	}
+
+	// init stats and time measure to zero
+	statistics.Reset()
+	startTime := time.Now()
+
+	over.AddGlobalFields("output-line")
+	err := ctx.pipeline.AddSink(jsonline.NewSinkWithContext(out, "output-line")).Run()
+
+	// include duration info and stats in log output
+	duration := time.Since(startTime)
+	over.MDC().Set("duration", duration)
+	stats := statistics.Compute()
+
+	over.SetGlobalFields([]string{"config", "output-line", "input-line", "duration"})
+	if err != nil {
+		return stats.WithErrorCode(4), fmt.Errorf("Pipeline didn't complete run: %v", err)
+	}
+
+	for name, path := range ctx.cfg.CachesToDump {
+		cache, ok := ctx.caches[name]
+		if !ok {
+			return stats.WithErrorCode(2), fmt.Errorf("Cache not found: %v", name)
+		}
+		err = dumpCache(name, cache, path, ctx.pdef.Caches[name].Reverse)
+		if err != nil {
+			return stats.WithErrorCode(3), fmt.Errorf("Cannot dump cache %v: %w", name, err)
+		}
+	}
+
+	return stats, nil
+}
+
+func dumpCache(name string, cache model.Cache, path string, reverse bool) error {
 	file, err := os.Create(path)
 	if err != nil {
 		return fmt.Errorf("Cache %s not dump : %s", name, err.Error())
@@ -59,7 +226,7 @@ func DumpCache(name string, cache model.Cache, path string, reverse bool) error 
 	return nil
 }
 
-func LoadCache(name string, cache model.Cache, path string, reverse bool) error {
+func loadCache(name string, cache model.Cache, path string, reverse bool) error {
 	file, err := os.Open(path)
 	if err != nil {
 		return fmt.Errorf("Cache %s not loaded : %s", name, err.Error())
@@ -93,4 +260,48 @@ func GetJsonSchema() (string, error) {
 		return "", err
 	}
 	return string(resBytes), nil
+}
+
+func injectMaskContextFactories() []model.MaskContextFactory {
+	return []model.MaskContextFactory{
+		fluxuri.Factory,
+		add.Factory,
+		addtransient.Factory,
+		remove.Factory,
+		pipe.Factory,
+		templateeach.Factory,
+		fromjson.Factory,
+	}
+}
+
+func injectMaskFactories() []model.MaskFactory {
+	return []model.MaskFactory{
+		constant.Factory,
+		command.Factory,
+		randomlist.Factory,
+		randomuri.Factory,
+		randomint.Factory,
+		weightedchoice.Factory,
+		regex.Factory,
+		hash.Factory,
+		randdate.Factory,
+		increment.Factory,
+		replacement.Factory,
+		duration.Factory,
+		templatemask.Factory,
+		rangemask.Factory,
+		randdura.Factory,
+		randomdecimal.Factory,
+		dateparser.Factory,
+		ff1.Factory,
+		luhn.Factory,
+		markov.Factory,
+	}
+}
+
+var re = regexp.MustCompile(`(\[\d*\])?$`)
+
+func updateContext(counter int) {
+	context := over.MDC().GetString("context")
+	over.MDC().Set("context", re.ReplaceAllString(context, fmt.Sprintf("[%d]", counter)))
 }
