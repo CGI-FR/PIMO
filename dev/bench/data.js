0f9b348fcfd452dcb2887f68296217502178dd63
github-action-benchmark <github@users.noreply.github.com>
2022-03-15 22:34:17 +0000

    add Benchmark (go) benchmark result for 2542f9aeab44b1a3599aeaf073b254b6fcd8a4e5

---
+++ dev/bench/data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1647383657544,
+  "repoUrl": "https://github.com/CGI-FR/PIMO",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "CGI-FR",
+            "username": "CGI-FR"
+          },
+          "committer": {
+            "name": "CGI-FR",
+            "username": "CGI-FR"
+          },
+          "id": "2542f9aeab44b1a3599aeaf073b254b6fcd8a4e5",
+          "message": "chore(perf): add bench tests",
+          "timestamp": "2022-02-07T17:15:00Z",
+          "url": "https://github.com/CGI-FR/PIMO/pull/101/commits/2542f9aeab44b1a3599aeaf073b254b6fcd8a4e5"
+        },
+        "date": 1647383657178,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkPimoRun",
+            "value": 204609,
+            "unit": "ns/op\t   16382 B/op\t     329 allocs/op",
+            "extra": "55201 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
