7108b10e2785314527ddbef525a12a56e1f0213b
github-action-benchmark <github@users.noreply.github.com>
2022-03-16 20:42:11 +0000

    add Benchmark (go) benchmark result for 189063e904eb2fe9b60678e446bd6c63a0c4db45

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1647383657544,
+  "lastUpdate": 1647463331876,
   "repoUrl": "https://github.com/CGI-FR/PIMO",
   "entries": {
     "Benchmark": [
@@ -26,6 +26,36 @@ window.BENCHMARK_DATA = {
             "value": 204609,
             "unit": "ns/op\t   16382 B/op\t     329 allocs/op",
             "extra": "55201 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "youen.peron@cgi.com",
+            "name": "Youen Péron",
+            "username": "youen"
+          },
+          "committer": {
+            "email": "youen.peron@cgi.com",
+            "name": "Youen Péron",
+            "username": "youen"
+          },
+          "distinct": true,
+          "id": "189063e904eb2fe9b60678e446bd6c63a0c4db45",
+          "message": "chore(bench): fix push on main",
+          "timestamp": "2022-03-16T20:39:27Z",
+          "tree_id": "fe14f13b1b4f9b3e3dafb7cd2f66ac8da4c8c6c4",
+          "url": "https://github.com/CGI-FR/PIMO/commit/189063e904eb2fe9b60678e446bd6c63a0c4db45"
+        },
+        "date": 1647463331233,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkPimoRun",
+            "value": 203084,
+            "unit": "ns/op\t   16491 B/op\t     343 allocs/op",
+            "extra": "54726 times\n2 procs"
           }
         ]
       }
