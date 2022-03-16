9e59e38fb4e117b35d0e1885c33dc99860bea226
Youen Péron <youen.peron@cgi.com>
2022-03-16 21:29:58 +0100

    chore(perf): add bench tests (#101)
    * chore(perf): add bench tests
    
    * chore: refresh go.mod
    
    * chore: enable auto-push
    
    * chore: add github token
    
    * chore: push perf result only for main
    
    * chore: fix variable name
    
    Co-authored-by: Adrien Aury <44274230+adrienaury@users.noreply.github.com>
    
    * chore: test commit
    
    Co-authored-by: Adrien Aury <adrien.aury@cgi.com>
    Co-authored-by: Adrien Aury <44274230+adrienaury@users.noreply.github.com>

--- internal/app/pimo/pimo_test.go
+++ internal/app/pimo/pimo_test.go
@@ -15,4 +15,74 @@
 // You should have received a copy of the GNU General Public License
 // along with PIMO.  If not, see <http://www.gnu.org/licenses/>.
 
-package pimo
+package pimo_test
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/cgi-fr/pimo/internal/app/pimo"
+	"github.com/cgi-fr/pimo/pkg/model"
+	"github.com/rs/zerolog"
+)
+
+func BenchmarkPimoRun(b *testing.B) {
+	definition := model.Definition{
+		Version: "1",
+		Masking: []model.Masking{
+			{
+				Selector: model.SelectorType{Jsonpath: "name"},
+				Mask: model.MaskType{
+					HashInURI: "pimo://nameFR",
+				},
+			},
+			{
+				Selector: model.SelectorType{Jsonpath: "familyName"},
+				Mask: model.MaskType{
+					HashInURI: "pimo://surnameFR",
+				},
+			},
+			{
+				Selector: model.SelectorType{Jsonpath: "domaine"},
+				Masks: []model.MaskType{
+					{Add: ""},
+					{RandomChoice: []model.Entry{"gmail.com", "msn.com"}},
+				},
+			},
+			{
+				Selector: model.SelectorType{Jsonpath: "email"},
+				Mask: model.MaskType{
+					Template: "{{.name}}.{{.familyName}}@{{.domaine}}",
+				},
+			},
+			{
+				Selector: model.SelectorType{Jsonpath: "domaine"},
+				Mask: model.MaskType{
+					Remove: true,
+				},
+			},
+		},
+	}
+
+	zerolog.SetGlobalLevel(zerolog.WarnLevel)
+
+	ctx := pimo.NewContext(definition)
+
+	data := model.NewDictionary().With("name", "John").With("familyName", "Doe").With("email", "john.doe@gmail.com")
+
+	cfg := pimo.Config{
+		EmptyInput:  false,
+		Iteration:   b.N,
+		SingleInput: &data,
+	}
+	err := ctx.Configure(cfg)
+	if err != nil {
+		b.FailNow()
+	}
+
+	b.ResetTimer()
+	_, err = ctx.Execute(ioutil.Discard)
+	if err != nil {
+		b.FailNow()
+	}
+}
