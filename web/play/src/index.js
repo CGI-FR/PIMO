7d0373c77f210f908e96ae01626d84334dcd1df0
Adrien Aury <44274230+adrienaury@users.noreply.github.com>
2022-05-31 16:56:09 +0200

    feat(play): client side (#116)
    * feat(play): basic client interface and data masking
    
    * feat(play): Several changes based on pull request comments
    
    * feat(play): lint validation changes
    
    * feat(play): fixed go.mod suppression issue
    
    * feat(play): fixed go.mod suppression issue V2
    
    Co-authored-by: Tibo Pfeifer <tibo.pfeifer@cgi.com>

---
+++ web/play/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import Play from "./Play";
+import 'bootstrap/dist/css/bootstrap.css'
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Play />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
