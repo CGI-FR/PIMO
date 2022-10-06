17d2d814c94efe4601a197ed08663f0ab6240cb4
Youen Péron <youen.peron-ext@pole-emploi.fr>
2022-10-06 15:48:43 +0000

    refactor(play): inject master version for dev

--- web/play/webpack.config.dev.js
+++ web/play/webpack.config.dev.js
@@ -4,5 +4,17 @@ import common from "./webpack.config.common.js";
 
 export default merge(common(false), {
   mode: "development",
-
+  module: {
+    rules: [
+      {
+        test: /index\.es$/,
+        loader: 'string-replace-loader',
+        options: {
+          search: '{{ version }}',
+          replace: 'master',
+          flags: 'g'
+        }
+      }
+    ]
+  }
 });
