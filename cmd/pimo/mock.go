93117f85a1c248389880c43441c36c8d68c47fef
Adrien Aury <adrien.aury@cgi.com>
2025-03-24 14:09:56 +0000

    feat(mock): add proxy, proxy-user and user flags

--- cmd/pimo/mock.go
+++ cmd/pimo/mock.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"net/url"
+	"os"
+	"strings"
 
 	over "github.com/adrienaury/zeromdc"
 	"github.com/cgi-fr/pimo/internal/app/pimo"
@@ -11,13 +13,30 @@ import (
 	"github.com/cgi-fr/pimo/pkg/uri"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
+	"golang.org/x/term"
 )
 
 // pimo --serve ":8080" --mask 'name=[{add: true}, {randomChoiceInUri: "pimo://nameFR"}]'
 // pimo -v5 mock -p :8081 http://localhost:8080
 
+func askPassword(prompt string) string {
+	if term.IsTerminal(int(os.Stdin.Fd())) {
+		os.Stdout.Write([]byte(prompt))
+		bytePassword, err := term.ReadPassword(int(os.Stdin.Fd()))
+		os.Stdout.Write([]byte("\n"))
+		if err != nil {
+			os.Exit(1)
+		}
+		return string(bytePassword)
+	}
+	return ""
+}
+
 func setupMockCommand(rootCmd *cobra.Command) {
 	mockAddr := ":8080"
+	proxyURL := ""
+	proxyAuth := ""
+	serverAuth := ""
 
 	mockCmd := &cobra.Command{
 		Use:   "mock",
@@ -34,12 +53,46 @@ func setupMockCommand(rootCmd *cobra.Command) {
 			if cmd.Flags().Changed("seed") {
 				globalSeed = &seedValue
 			}
-			runMockCommand(backendURL, mockAddr, mockConfigFile, globalSeed)
+
+			client := http.DefaultClient
+			if proxyURL != "" {
+				proxyURLParsed, err := url.Parse(proxyURL)
+				if err != nil {
+					log.Fatal().Err(err).Msg("Failed to parse proxy URL")
+				}
+				if proxyAuth != "" {
+					if !strings.Contains(proxyAuth, ":") {
+						password := askPassword("enter password for proxy authentication: ")
+						proxyAuth = proxyAuth + ":" + password
+					}
+					proxyURLParsed.User = url.UserPassword(strings.Split(proxyAuth, ":")[0], strings.Split(proxyAuth, ":")[1])
+				}
+				client.Transport = &http.Transport{
+					Proxy: http.ProxyURL(proxyURLParsed),
+				}
+			}
+
+			if serverAuth != "" {
+				if !strings.Contains(serverAuth, ":") {
+					password := askPassword("enter password for server authentication: ")
+					proxyAuth = proxyAuth + ":" + password
+				}
+				client.Transport = &TransportWithAuth{
+					wrapped: client.Transport,
+					user:    strings.Split(serverAuth, ":")[0],
+					pass:    strings.Split(serverAuth, ":")[1],
+				}
+			}
+
+			runMockCommand(backendURL, mockAddr, mockConfigFile, globalSeed, client)
 		},
 		Args: cobra.ExactArgs(1),
 	}
 
 	mockCmd.PersistentFlags().StringVarP(&mockAddr, "port", "p", mockAddr, "address and port number")
+	mockCmd.PersistentFlags().StringVarP(&proxyURL, "proxy", "x", proxyURL, "use the specified proxy to perform backend request")
+	mockCmd.PersistentFlags().StringVarP(&proxyAuth, "proxy-user", "U", proxyAuth, "specify user:password to use for proxy authentication")
+	mockCmd.PersistentFlags().StringVarP(&serverAuth, "user", "u", serverAuth, "specify user:password to use for server authentication")
 	addFlag(mockCmd, flagBufferSize)
 	// addFlag(mockCmd, flagCatchErrors) // could use
 	addFlag(mockCmd, flagConfigRoute)
@@ -56,7 +109,21 @@ func setupMockCommand(rootCmd *cobra.Command) {
 	rootCmd.AddCommand(mockCmd)
 }
 
-func runMockCommand(backendURL, mockAddr, configFile string, globalSeed *int64) {
+type TransportWithAuth struct {
+	wrapped http.RoundTripper
+	user    string
+	pass    string
+}
+
+func (t *TransportWithAuth) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.SetBasicAuth(t.user, t.pass)
+	if t.wrapped == nil {
+		return http.DefaultTransport.RoundTrip(req)
+	}
+	return t.wrapped.RoundTrip(req)
+}
+
+func runMockCommand(backendURL, mockAddr, configFile string, globalSeed *int64, client *http.Client) {
 	pimo.InjectMasks()
 	statistics.Reset()
 
@@ -72,7 +139,7 @@ func runMockCommand(backendURL, mockAddr, configFile string, globalSeed *int64) 
 		log.Fatal().Err(err).Msg("Failed to parse backend URL")
 	}
 
-	ctx, err := cfg.Build(backend, globalSeed, cachesToLoad)
+	ctx, err := cfg.Build(backend, globalSeed, cachesToLoad, client)
 	if err != nil {
 		log.Fatal().Err(err).Msgf("Failed to build routes from %s", configFile)
 	}
